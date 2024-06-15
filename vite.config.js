8c82e2bf4facbf48d45aaaa1339f53a32fc24950
wangtao <wangtao970503@foxmail.com>
2024-06-15 17:10:22 +0800

    项目启动时打开浏览器

--- vite.config.js
+++ vite.config.js
@@ -21,9 +21,10 @@ export default defineConfig({
     }
   },
   server: {
+    open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
