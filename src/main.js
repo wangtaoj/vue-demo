5b6444bbd84c57a39ce04ebc982ed13999bc3c6b
wangtao <wangtao970503@foxmail.com>
2024-11-24 12:49:05 +0800

    增加Vue实例参数解释

--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ import './assets/main.css';
 
 Vue.use(Antd);
 
+/**
+ * el: Vue 实例的挂载目标, 一个真实存在的dom节点, 可以使用CSS选择器, 也可以是一个HTMLElement实例
+ * template: 模板字符串, 模板将会替换挂载的元素
+ * render: 字符串模板的代替方案
+ *
+ * 若没有指定el选项, 则需要手动调用$mount()函数指定挂载节点
+ * 若只有el选项, 没有指定template或者render函数, 则挂载dom节点的html会被提取出来用作模板
+ */
 new Vue({
   router,
   render(h) {
